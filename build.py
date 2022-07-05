5300642e87441b06c0ad543cfc7d6a11058c4570
zhou <zhoutjcc@msn.cn>
2022-07-05 15:36:48 +0800

    apply --ks-key-alias

--- build.py
+++ build.py
@@ -12,6 +12,7 @@ class BuildOptions:
         self.parser.add_argument("--path", type=str, required=True, help="aab的路径")
         self.parser.add_argument("--password", type=str, required=True, help="你自己的密码")
         self.parser.add_argument("--jks_path", type=str, required=True, help="你自己jks的地址")
+        self.parser.add_argument("--alias", type=str, default="release", help="你自己jks的地址")
 
     def parse(self):
         return self.parser.parse_args()
@@ -54,7 +55,7 @@ if __name__ == '__main__':
     query_by_jarsigner(
         '-verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore ' + jks_path + ' ' + aab_path + ' release')
     query_by_java_jar('./bundletool-all-1.9.1.jar',
-                      'build-apks --bundle=' + aab_path + ' --output=' + apks_path + ' --mode=universal --ks=' + jks_path + ' --ks-pass=pass:' + password + ' --ks-key-alias=release --key-pass=pass:' + password)
+                      'build-apks --bundle=' + aab_path + ' --output=' + apks_path + ' --mode=universal --ks=' + jks_path + ' --ks-pass=pass:' + password + ' --ks-key-alias=' + args.alias + ' --key-pass=pass:' + password)
     zip_path = apks_path.replace('.apks', '.zip')
     os.renames(apks_path, zip_path)
     zFile = zipfile.ZipFile(zip_path, "r")
@@ -65,7 +66,8 @@ if __name__ == '__main__':
     for fileM in zFile.namelist():
         zFile.extract(fileM, zip_apk_path)
 
-    os.renames(zip_apk_path + '/universal.apk',
-               zip_apk_path + os.sep + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '_' + apks_path.replace(
-                   '.apks', '.apk'))
+    rename = zip_apk_path + os.sep + time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()) + '_' + apks_path.replace(
+        '.apks', '.apk')
+    os.renames(zip_apk_path + '/universal.apk', rename)
+    print("output apk", rename)
     zFile.close()
